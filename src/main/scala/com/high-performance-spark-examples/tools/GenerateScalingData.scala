5c963ffa4131d76281776357425050d62e8e0fcc
Holden Karau <holden@pigscanfly.ca>
2015-10-11 12:43:24 -0700

    Add a quick function for generating some test like data

---
+++ src/main/scala/com/high-performance-spark-examples/tools/GenerateScalingData.scala
@@ -0,0 +1,15 @@
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.random.RandomRDDs
+
+object GenerateScalingData {
+  /**
+   * Generate a Goldilocks data set. We expect the key to follow an exponential
+   * distribution and the data its self to be normal.
+   */
+  def generateGoldilocks(sc: SparkContext, size: Long): RDD[List[String]] = {
+    val keyRDD = RandomRDDs.exponentialRDD(sc, mean = 1000,  size = size).map(_.toInt)
+    val valuesRDD = RandomRDDs.normalVectorRDD(sc, numRows = 1, numCols = 500)
+    keyRDD.zip(valuesRDD).map{case (k, v) => List(k.toString) ++ v.toArray.map(_.toString)}
+  }
+}
