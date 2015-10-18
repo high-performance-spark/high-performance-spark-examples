923fb3334c44117d0977bca040c05e48d66f1760
Holden Karau <holden@pigscanfly.ca>
2015-10-17 17:09:18 -0700

    Add createHiveContext and createSqlContext

--- src/main/scala/com/high-performance-spark-examples/dataframe/HappyPandas.scala
+++ src/main/scala/com/high-performance-spark-examples/dataframe/HappyPandas.scala
@@ -1,9 +1,32 @@
 /**
  * Happy Panda Example for DataFrames. Computes the % of happy pandas. Very contrived.
  */
+
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.hive._
 import org.apache.spark.sql.DataFrame
 
 object HappyPanda {
+  // How to create a HiveContext or SQLContext with an existing SparkContext
+  def sqlContext(sc: SparkContext): SQLContext = {
+    //tag::createSQLContext[]
+    val sqlContext = new SQLContext(sc)
+    // Import the implicits, unlike in core Spark the implicits are defined on the context
+    import sqlContext.implicits._
+    //end::createSQLContext[]
+    sqlContext
+  }
+
+  def hiveContext(sc: SparkContext): HiveContext = {
+    //tag::createHiveContext[]
+    val hiveContext = new HiveContext(sc)
+    // Import the implicits, unlike in core Spark the implicits are defined on the context
+    import hiveContext.implicits._
+    //end::createHiveContext[]
+    hiveContext
+  }
+
   def happyPandas(pandaInfo: DataFrame): DataFrame = {
     pandaInfo.select(pandaInfo("place"),
       (pandaInfo("happyPandas") / pandaInfo("totalPandas")).as("percentHappy"))
