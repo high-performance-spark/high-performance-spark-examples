4fa1643e57de88a12074ed795ddf77f8514e1aca
Holden Karau <holden@pigscanfly.ca>
2015-10-21 00:03:14 -0700

    Add a few more spark sql examples

--- src/main/scala/com/high-performance-spark-examples/dataframe/HappyPandas.scala
+++ src/main/scala/com/high-performance-spark-examples/dataframe/HappyPandas.scala
@@ -6,6 +6,8 @@ import org.apache.spark._
 import org.apache.spark.sql._
 import org.apache.spark.sql.hive._
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.catalyst.expressions.aggregate._
+import org.apache.spark.sql.functions._
 
 object HappyPanda {
   // How to create a HiveContext or SQLContext with an existing SparkContext
@@ -31,7 +33,32 @@ object HappyPanda {
     pandaInfo.select(pandaInfo("place"),
       (pandaInfo("happyPandas") / pandaInfo("totalPandas")).as("percentHappy"))
   }
+
   def minHappyPandas(pandaInfo: DataFrame, num: Int): DataFrame = {
     pandaInfo.filter(pandaInfo("happyPandas") >= num)
   }
+
+  //tag::maxPandaSizePerZip[]
+  def maxPandaSizePerZip(pandas: DataFrame): DataFrame = {
+    pandas.groupBy(pandas("zip")).max("pandasize")
+  }
+  //end::maxPandaSizePerZip[]
+
+  //tag::minMaxPandasSizePerZip[]
+  def minMaxPandaSizePerZip(pandas: DataFrame): DataFrame = {
+    // List of strings
+    pandas.groupBy(pandas("zip")).agg(("min", "pandasize"), ("max", "pandasize"))
+    // Map of column to aggregate
+    pandas.groupBy(pandas("zip")).agg(Map("pandasize" -> "min",
+      "pandasize" -> "max"))
+    // expression literals
+  }
+  //end::minMaxPandasSizePerZip[]
+
+  //tag::complexAggPerZip[]
+  def complexAggPerZip(pandas: DataFrame): DataFrame = {
+    // Compute the min and mean
+    pandas.groupBy(pandas("zip")).agg(min(pandas("pandasize")), mean(pandas("pandasize")))
+  }
+  //end::complexAggPerZip[]
 }
