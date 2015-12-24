2bc36e088c7d5dcc5b591a88c2d784767ce73cd1
Mahmoud Hanafy <mahmoud.hanafy@badrit.com>
2015-12-24 17:07:14 +0200

    Add pandaInfo test cases

--- src/test/scala/com/high-performance-spark-examples/dataframe/HappyPandas.scala
+++ src/test/scala/com/high-performance-spark-examples/dataframe/HappyPandas.scala
@@ -3,39 +3,41 @@
  */
 package com.highperformancespark.examples.dataframe
 
-import org.apache.spark.sql.{SQLContext, DataFrame, Row}
-import org.apache.spark.sql.types._
-
+import com.highperformancespark.examples.dataframe.HappyPanda.PandaInfo
 import com.holdenkarau.spark.testing._
-
-import org.scalatest.FunSuite
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.scalatest.Matchers._
-import org.scalatest.exceptions.TestFailedException
 
-class HappyPandasTest extends FunSuite with SharedSparkContext with DataFrameSuiteBase {
-  val inputList = List(PandaInfo("toronto", 2, 1), PandaInfo("san diego", 3, 2))
+class HappyPandasTest extends DataFrameSuiteBase {
+  val toronto = "toronto"
+  val sandiego = "san diego"
+  val virginia = "virginia"
+  val pandInfoList = List(PandaInfo(toronto, "giant", 1, 2),
+                          PandaInfo(sandiego, "red", 2, 3),
+                          PandaInfo(virginia, "black", 1, 10))
 
   //tag::approxEqualDataFrames[]
-  test("verify simple happy pandas") {
-    val sqlCtx = sqlContext
-    import sqlCtx.implicits._
-    val expectedResult = List(Row("toronto", 0.5), Row("san diego", 2/3.0))
-    val expectedDf = sqlCtx.createDataFrame(sc.parallelize(expectedResult),
-      StructType(List(StructField("place", StringType),
-        StructField("percentHappy", DoubleType))))
-    val inputDF = sqlCtx.createDataFrame(inputList)
-    val result = HappyPanda.happyPandas(inputDF)
+
+  test("verify simple happy pandas Percentage") {
+    val expectedResult = List(Row(toronto, 0.5), Row(sandiego, 2/3.0), Row(virginia, 1/10.0))
+    val expectedDf = createDF(expectedResult, ("place", StringType),
+                                              ("percentHappy", DoubleType))
+
+    val inputDF = sqlContext.createDataFrame(pandInfoList)
+    val result = HappyPanda.happyPandasPercentage(inputDF)
+
     approxEqualDataFrames(expectedDf, result, 1E-5)
   }
   //end::approxEqualDataFrames[]
 
   test("verify approx by hand") {
-    val sqlCtx = sqlContext
-    import sqlCtx.implicits._
-    val expectedRows = List(Row("toronto", 0.5), Row("san diego", 2/3.0))
-    val inputDF = sqlCtx.createDataFrame(inputList)
-    val result = HappyPanda.happyPandas(inputDF)
-    val resultRows = result.collect()
+    val inputDF = sqlContext.createDataFrame(pandInfoList)
+    val resultDF = HappyPanda.happyPandasPercentage(inputDF)
+    val resultRows = resultDF.collect()
+
+    val expectedRows = List(Row(toronto, 0.5), Row(sandiego, 2/3.0), Row(virginia, 1/10.0))
+
     //tag::approxEqualRow[]
     assert(expectedRows.size === resultRows.size)
     expectedRows.zip(resultRows).foreach{case (r1, r2) =>
@@ -45,21 +47,43 @@ class HappyPandasTest extends FunSuite with SharedSparkContext with DataFrameSui
     //end::approxEqualRow[]
   }
 
+  test("test encode Panda type") {
+    val inputDF = sqlContext.createDataFrame(pandInfoList)
+    val resultDF = HappyPanda.encodePandaType(inputDF)
+
+    val expectedRows = List(Row(toronto, 0), Row(sandiego, 1), Row(virginia, 2))
+    val expectedDF = createDF3(expectedRows, ("place", StringType, true),
+                                             ("encodedType", IntegerType, false))
+
+    equalDataFrames(expectedDF, resultDF)
+  }
+
   //tag::exactEqualDataFrames[]
   test("verify exact equality") {
-    val sqlCtx = sqlContext
-    import sqlCtx.implicits._
-    val inputDF = sqlCtx.createDataFrame(inputList)
+    // test minHappyPandas
+    val inputDF = sqlContext.createDataFrame(pandInfoList)
     val result = HappyPanda.minHappyPandas(inputDF, 2)
     val resultRows = result.collect()
-    assert(List(Row("san diego", 3, 2)) === resultRows)
+
+    val expectedRows = List(Row(sandiego, "red", 2, 3))
+    assert(expectedRows === resultRows)
   }
   //end::exactEqualDataFrames[]
+
+  test("test happyPandasPlaces") {
+    val inputDF = sqlContext.createDataFrame(pandInfoList)
+    val resultDF = HappyPanda.happyPandasPlaces(inputDF)
+
+    val expectedRows = List(PandaInfo(toronto, "giant", 1, 2),
+                            PandaInfo(sandiego, "red", 2, 3))
+    val expectedDF = sqlContext.createDataFrame(expectedRows)
+
+    equalDataFrames(expectedDF, resultDF)
+  }
 
   // Make a test once we have hivectx in the base
   def futureTestRrelativePandaSize() {
     val sqlCtx = sqlContext
-    import sqlCtx.implicits._
     // TODO: Generate some data instead of using the small static data
     val inputDF = loadPandaStuffies(sqlCtx)
     val result = HappyPanda.computeRelativePandaSizes(inputDF)
@@ -78,6 +102,18 @@ class HappyPandasTest extends FunSuite with SharedSparkContext with DataFrameSui
       StructField("zip", IntegerType, true)))
     sqlCtx.createDataFrame(sc.parallelize(pandaStuffies), schema)
   }
-}
 
-case class PandaInfo(place: String, totalPandas: Integer, happyPandas: Integer)
+
+  private def createDF(list: List[Row], fields: Tuple2[String, DataType]*) =
+    sqlContext.createDataFrame(sc.parallelize(list), structType2(fields))
+
+  private def structType2(fields: Seq[(String, DataType)]) =
+    StructType(fields.map(f => (StructField(f._1, f._2))).toList)
+
+
+  private def createDF3(list: List[Row], fields: Tuple3[String, DataType, Boolean]*) =
+    sqlContext.createDataFrame(sc.parallelize(list), structType3(fields))
+
+  private def structType3(fields: Seq[(String, DataType, Boolean)]) =
+    StructType(fields.map(f => (StructField(f._1, f._2, f._3))).toList)
+}
