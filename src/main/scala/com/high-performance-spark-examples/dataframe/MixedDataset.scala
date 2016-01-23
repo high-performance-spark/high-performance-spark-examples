93152b176ffd55bcd1dd47708017b083a5215f97
Holden Karau <holden@us.ibm.com>
2016-01-22 23:07:28 -0800

    Fill in some stuff

--- src/main/scala/com/high-performance-spark-examples/dataframe/MixedDataset.scala
+++ src/main/scala/com/high-performance-spark-examples/dataframe/MixedDataset.scala
@@ -4,7 +4,8 @@
 package com.highperformancespark.examples.dataframe
 
 import org.apache.spark._
-import org.apache.spark.sql.{Dataset, SQLContext}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.expressions.aggregate._
 import org.apache.spark.sql.expressions._
 import org.apache.spark.sql.functions._
@@ -13,46 +14,54 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.hive._
 import org.apache.spark.sql.hive.thriftserver._
 
-object MixedDataset {
+class MixedDataset(sqlCtx: SQLContext) {
+  import sqlCtx.implicits._
+
   /**
    * A sample function on a Dataset of RawPandas.
    * This is contrived, since our reduction could also be done with SQL aggregates, but
    * we can see the flexibility of being able to specify arbitrary Scala code.
    */
-  def happyPandaSums() = {
+  def happyPandaSums(ds: Dataset[RawPanda]): Double = {
+    ds.toDF().filter($"happy" === true).as[RawPanda].
+      select($"attributes"(0).as[Double]).
+      reduce((x, y) => x + y)
   }
 
   /**
-   * A sample function on a Dataset of RawPandas that round trips through a DataFrame for sorting.
-   * In the future sorting may be available directly on Datasets but this example illustrates how to
-   * convert from
+   * Functional map + Dataset, sums the positive attributes for the pandas
    */
-  def topHappyPandasSums() = {
+  def funMap(ds: Dataset[RawPanda]): Dataset[Double] = {
+    ds.map{rp => rp.attributes.filter(_ > 0).sum}
   }
 
   /**
    * Illustrate how we make typed queries, using some of the float properties to produce boolean
    * values.
    */
-  def typedQueryExample() = {
+  def typedQueryExample(ds: Dataset[RawPanda]): Dataset[Double] = {
+    ds.select($"attributes"(0).as[Double])
   }
 
   /**
    * Illustrate converting a Dataset to an RDD
    */
-  def toRDD() = {
+  def toRDD(ds: Dataset[RawPanda]): RDD[RawPanda] = {
+    ds.rdd
   }
 
   /**
    * Illustrate converting a Dataset to a DataFrame
    */
-  def toDF() = {
+  def toDF(ds: Dataset[RawPanda]): DataFrame = {
+    ds.toDF()
   }
 
   /**
    * Illustrate DataFrame to Dataset. Its important to note that if the schema does not match what
    * is expected by the Dataset this fails fast.
    */
-  def fromDF() = {
+  def fromDF(df: DataFrame): Dataset[RawPanda] = {
+    df.as[RawPanda]
   }
 }
