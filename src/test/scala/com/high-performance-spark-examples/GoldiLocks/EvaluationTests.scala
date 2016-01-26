80ebcbb6c551790d8c564cca6304b6d8f2caad39
Rachel <rachel@alpinenow.com>
2016-01-26 15:04:51 -0800

    adding caching example

--- src/test/scala/com/high-performance-spark-examples/GoldiLocks/EvaluationTests.scala
+++ src/test/scala/com/high-performance-spark-examples/GoldiLocks/EvaluationTests.scala
@@ -5,11 +5,12 @@ import org.apache.spark.rdd.RDD
 import org.scalatest.FunSuite
 
 class EvaluationTests extends FunSuite with SharedSparkContext {
-
+  val doubleList = Array(1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0, 4.0)
+  val keyValuePairs =  Array(1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0, 4.0).zipWithIndex
+  val path = "target/testResults"
   test("MapValues preserves Partitioning "){
-    val s = Array(1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0, 4.0).zipWithIndex
-    val data: RDD[(Double, Int )] = sc.parallelize(s)
 
+    val data: RDD[(Double, Int )] = sc.parallelize(keyValuePairs)
     // tag::MapValues[]
     val sortedData = data.sortByKey()
     val mapValues: RDD[(Double, String)] = sortedData.mapValues(_.toString)
@@ -27,6 +28,27 @@ class EvaluationTests extends FunSuite with SharedSparkContext {
     val rddB = sc.parallelize(b)
     val rddC =  rddA.subtract(rddB)
     assert(rddC.count() < rddA.count() - rddB.count())
+  }
+
+  test( "Two actions without caching  ") {
+    val rddA: RDD[(Double, Int)] = sc.parallelize(keyValuePairs)
+
+    // tag::TwoActions[]
+    val sorted = rddA.sortByKey()
+    val count = sorted.count()
+    sorted.saveAsHadoopFile(path)
+    // end::TwoActions[]
+  }
+
+  test( "Two actions with caching  "){
+    val rddA: RDD[(Double, Int)] = sc.parallelize(keyValuePairs)
+    // tag::TwoActionsCache[]
+    val sorted = rddA.sortByKey()
+    val count = sorted.count()
+    rddA.persist()
+    sorted.saveAsHadoopFile(path)
+    // end::TwoActionsCache[]
+
   }
 
 }
