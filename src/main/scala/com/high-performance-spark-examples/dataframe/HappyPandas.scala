a2f6a2391523dc4eabdebac13351421ad8c34e1e
Mahmoud Hanafy <mahmooud.hanafy@gmail.com>
2016-02-01 22:37:39 +0200

    Resolve code inspection issues

--- src/main/scala/com/high-performance-spark-examples/dataframe/HappyPandas.scala
+++ src/main/scala/com/high-performance-spark-examples/dataframe/HappyPandas.scala
@@ -5,19 +5,18 @@ package com.highperformancespark.examples.dataframe
 
 import org.apache.spark._
 //tag::sparkSQLImports[]
-import org.apache.spark.sql.{DataFrame, SQLContext, Row}
-import org.apache.spark.sql.catalyst.expressions.aggregate._
+import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.sql.expressions._
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types._
 //end::sparkSQLImports[]
+
 //tag::sparkHiveImports[]
 // Additional imports for using HiveContext
 import org.apache.spark.sql.hive._
 import org.apache.spark.sql.hive.thriftserver._
 //end::sparkHiveImports[]
 
-object HappyPanda {
+object HappyPandas {
   // create SQLContext with an existing SparkContext
   def sqlContext(sc: SparkContext): SQLContext = {
     //tag::createSQLContext[]
@@ -205,8 +204,8 @@ object HappyPanda {
     //end::relativePandaSizesWindow[]
 
     //tag::relativePandaSizesQuery[]
-    val pandaRelativeSizeFunc = (pandas("pandaSize") -
-      avg(pandas("pandaSize")).over(windowSpec))
+    val pandaRelativeSizeFunc = pandas("pandaSize") -
+      avg(pandas("pandaSize")).over(windowSpec)
 
     pandas.select(pandas("name"), pandas("zip"), pandas("pandaSize"), pandas("age"),
       pandaRelativeSizeFunc.as("panda_relative_size"))
