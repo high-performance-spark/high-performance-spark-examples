9e01fc7e1dad3eb2553aff8523e2f47feee8ff81
Holden Karau <holden@us.ibm.com>
2016-03-16 21:28:22 -0700

    Long line break

--- src/main/scala/com/high-performance-spark-examples/wordcount/WordCount.scala
+++ src/main/scala/com/high-performance-spark-examples/wordcount/WordCount.scala
@@ -31,7 +31,8 @@ object WordCount {
   //tag::wordCountStopwords[]
   def withStopWordsFiltered(rdd : RDD[String], illegalTokens : Array[Char],
     stopWords : Set[String]): RDD[(String, Int)] = {
-    val tokens: RDD[String] = rdd.flatMap(_.split(illegalTokens ++ Array[Char](' ')).map(_.trim.toLowerCase))
+    val tokens: RDD[String] = rdd.flatMap(_.split(illegalTokens ++ Array[Char](' ')).
+      map(_.trim.toLowerCase))
     val words = tokens.filter(token =>
       !stopWords.contains(token) && (token.length > 0) )
     val wordPairs = words.map((_, 1))
