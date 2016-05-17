0c94c7dfddeb4f70ba4570eb71f27db701bd223a
Holden Karau <holden@us.ibm.com>
2016-05-17 12:18:26 -0700

    Fix sample scala code

--- src/main/scala/com/high-performance-spark-examples/native/NativeExample.scala
+++ src/main/scala/com/high-performance-spark-examples/native/NativeExample.scala
@@ -2,6 +2,8 @@ package com.highperformancespark.examples.ffi
 
 import org.apache.spark.rdd.RDD
 
+import ch.jodersky.jni.nativeLoader
+
 @nativeLoader("libhighPerformanceSpark0")
 object NativeExample {
   def jniSum(input: RDD[(String, Array[Int])]): RDD[(String, Int)] = {
