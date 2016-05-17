5f452d890fde8039f24055a6c83452b4a30753b4
Holden Karau <holden@us.ibm.com>
2016-05-17 14:26:57 -0700

    Use native loader magics

--- src/main/c/sum.h
+++ src/main/c/sum.h
@@ -1,1 +1,6 @@
+#ifndef _SUM_H
+#define _SUM_H
+
 int sum(int input[], int num_elem);
+
+#endif /* _SUM_H */
--- src/main/scala/com/high-performance-spark-examples/native/NativeExample.scala
+++ src/main/scala/com/high-performance-spark-examples/native/NativeExample.scala
@@ -2,9 +2,6 @@ package com.highperformancespark.examples.ffi
 
 import org.apache.spark.rdd.RDD
 
-import ch.jodersky.jni.nativeLoader
-
-@nativeLoader("libhighPerformanceSpark0")
 object NativeExample {
   def jniSum(input: RDD[(String, Array[Int])]): RDD[(String, Int)] = {
     input.mapValues(values => new SumJNI().sum(values))
