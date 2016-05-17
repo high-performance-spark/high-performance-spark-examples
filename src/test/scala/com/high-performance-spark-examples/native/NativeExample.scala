77b53a206df0b65f03264020b5845d49b65b69bf
Holden Karau <holden@us.ibm.com>
2016-05-16 18:30:34 -0700

    Add more test magic

---
+++ src/test/scala/com/high-performance-spark-examples/native/NativeExample.scala
@@ -0,0 +1,37 @@
+/**
+ * Test our simple JNI
+ */
+package com.highperformancespark.examples.ffi
+
+import com.holdenkarau.spark.testing._
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Prop.forAll
+import org.scalatest.FunSuite
+import org.scalatest.prop.Checkers
+import org.scalatest.Matchers._
+
+class NativeExampleSuite extends FunSuite with SharedSparkContext with Checkers {
+  test("local sum") {
+  //def magic2() {
+    val input = Array(1, 2, 3)
+    val sumMagic = new SumJNI()
+    val result = sumMagic.sum(input)
+    val expected = 6
+    result === expected
+  }
+
+  //  test("super simple test") {
+  def magic() {
+    val input = sc.parallelize(List(("hi", Array(1, 2, 3))))
+    val result = NativeExample.jniSum(input).collect()
+    val expected = List(("hi", 6))
+    result === expected
+  }
+
+  test("native call should find sum correctly") {
+    val property = forAll(RDDGenerator.genRDD[(String, Array[Int])](sc)) {
+      rdd => rdd.mapValues(_.sum).collect() === NativeExample.jniSum(rdd)
+    }
+    //check(property)
+  }
+}
