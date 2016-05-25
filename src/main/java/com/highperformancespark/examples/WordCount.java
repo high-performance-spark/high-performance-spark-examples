62ebfe52ca7f521b798221266d8ab9ba0f357728
Holden Karau <holden@us.ibm.com>
2016-05-25 11:24:44 -0700

    Add WordCount Java example and update gitignore since we have python stuff now

---
+++ src/main/java/com/highperformancespark/examples/WordCount.java
@@ -0,0 +1,24 @@
+package com.highperformancespark.examples;
+
+//tag::wordCount[]
+import scala.Tuple2;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.regex.Pattern;
+import java.util.Arrays;
+
+public final class WordCount {
+  private static final Pattern pattern = Pattern.compile(" ");
+
+  public static void main(String[] args) throws Exception {
+    JavaSparkContext jsc = new JavaSparkContext();
+    JavaRDD<String> lines = jsc.textFile(args[0]);
+    JavaRDD<String> words = lines.flatMap(e -> Arrays.asList(pattern.split(e)));
+    JavaPairRDD<String, Integer> wordsIntial = words.mapToPair(
+      e -> new Tuple2<String, Integer>(e, 1));                                                         
+  }
+}
+//end::wordCount[]
