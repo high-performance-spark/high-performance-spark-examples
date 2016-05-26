0083cf001b5a9dd447291f40387c0c03c33e1c9f
Holden Karau <holden@us.ibm.com>
2016-05-25 17:24:59 -0700

    Add tags for sumJNI scala example

--- src/main/scala/com/high-performance-spark-examples/native/SumJNI.scala
+++ src/main/scala/com/high-performance-spark-examples/native/SumJNI.scala
@@ -3,6 +3,8 @@ package com.highperformancespark.examples.ffi
 import ch.jodersky.jni.nativeLoader
 
 @nativeLoader("high-performance-spark0")
+// tag::sumJNI[]
 class SumJNI {
   @native def sum(n: Array[Int]): Int
 }
+// end::sumJNI[]
