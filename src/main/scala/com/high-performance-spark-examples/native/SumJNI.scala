430a9d4eef696dbad6f3631f283feb2375fc3b4a
Holden Karau <holden@pigscanfly.ca>
2016-05-28 16:33:00 -0700

    More Fortran magic I guess

--- src/main/scala/com/high-performance-spark-examples/native/SumJNI.scala
+++ src/main/scala/com/high-performance-spark-examples/native/SumJNI.scala
@@ -2,7 +2,9 @@ package com.highperformancespark.examples.ffi
 
 import ch.jodersky.jni.nativeLoader
 
+//tag::sumJNIDecorator[]
 @nativeLoader("high-performance-spark0")
+//end::sumJNIDecorator[]
 // tag::sumJNI[]
 class SumJNI {
   @native def sum(n: Array[Int]): Int
