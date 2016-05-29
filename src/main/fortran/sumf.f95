88d5ffa4031f1e96e8d066c15e29ab10154d7270
Holden Karau <holden@pigscanfly.ca>
2016-05-28 18:57:34 -0700

    oooh fuck that was pass by ref and I didn't notice

--- src/main/fortran/sumf.f95
+++ src/main/fortran/sumf.f95
@@ -1,4 +1,8 @@
-       INTEGER FUNCTION SUMF(N,A)
-       DIMENSION A(N)
-       SUMF=SUM(A)
+       INTEGER FUNCTION SUMF(N,A) BIND(C, NAME='sumf')
+       INTEGER A(N)
+       ISUM=0
+       DO I=1, N
+         ISUM=ISUM+A(I)
+       ENDDO
+       SUMF=ISUM
        END
