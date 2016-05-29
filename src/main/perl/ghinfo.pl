f0d9157ba5b625db5baaaecb455966a157cfd92a
Holden Karau <holden@us.ibm.com>
2016-05-29 15:13:24 -0700

    Fix up perl example

--- src/main/perl/ghinfo.pl
+++ src/main/perl/ghinfo.pl
@@ -4,16 +4,17 @@ use warnings;
 
 use Pithub;
 use Data::Dumper;
-use File::Slurp;
 
-# Find all of the commentors on a PR
+# Find all of the commentors on an issue
 my $user = $ENV{'user'};
 my $repo = $ENV{'repo'};
 my $p = Pithub->new(user => $user, repo => $repo);
 while (my $id = <>) {
     chomp ($id);
-    my $pr_comments(p->pull_requests->comments->list(pull_request_id => $id));
+    my $issue_comments = $p->issues->comments->list(issue_id => $id);
+    print $id;
     while (my $comment = $issue_comments->next) {
-	print $comment->{"user"}->{"login"}."\n";
+	print " ".$comment->{"user"}->{"login"};
     }
+    print "\n";
 }
