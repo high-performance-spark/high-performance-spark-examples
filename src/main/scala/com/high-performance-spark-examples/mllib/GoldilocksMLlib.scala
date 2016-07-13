6bdb0740fc83690230d5f952b6a4b522439fc038
Holden Karau <holden@us.ibm.com>
2016-07-13 11:21:40 -0700

    Add some tags and zip based hasing preserve

--- src/main/scala/com/high-performance-spark-examples/mllib/GoldilocksMLlib.scala
+++ src/main/scala/com/high-performance-spark-examples/mllib/GoldilocksMLlib.scala
@@ -7,10 +7,13 @@ import scala.collection.mutable.{ArrayBuffer, MutableList}
 
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
+//tag::imports[]
 import org.apache.spark.mllib.linalg.Vectors
+// Rename Vector to SparkVector to avoid conflicts with Scala's Vector class
 import org.apache.spark.mllib.linalg.{Vector => SparkVector}
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.feature._
+import org.apache.spark.mllib.feature.HashingTF
+//end::imports[]
 
 class GoldilocksMLlib(sc: SparkContext) {
 
@@ -41,11 +44,33 @@ class GoldilocksMLlib(sc: SparkContext) {
     }
   }
 
+  //tag::hashingTFSimple[]
   def hashingTf(rdd: RDD[String]): RDD[SparkVector] = {
     val ht = new HashingTF()
     val tokenized = rdd.map(_.split(" ").toIterable)
     ht.transform(tokenized)
   }
+  //end::hashingTFSimple[]
+
+  //tag::hashingTFPreserve[]
+  def toVectorPerserving(rdd: RDD[RawPanda]): RDD[(RawPanda, SparkVector)] = {
+    val ht = new HashingTF()
+    rdd.map{panda =>
+      val textField = panda.pt
+      val tokenizedTextField = textField.split(" ").toIterable
+      (panda, ht.transform(tokenizedTextField))
+    }
+  }
+  //end::hashingTFPreserve[]
+
+  //tag::hashingTFPreserveZip[]
+  def hashingTFPreserveZip(rdd: RDD[RawPanda]): RDD[(RawPanda, SparkVector)] = {
+    val ht = new HashingTF()
+    val tokenized = rdd.map{panda => panda.pt.split(" ").toIterable}
+    val vecs = ht.transform(tokenized)
+    rdd.zip(vecs)
+  }
+  //end::hashingTFPreserveZip[]
 
   def toLabeledPointWithHashing(rdd: RDD[RawPanda]): RDD[LabeledPoint] = {
     val ht = new HashingTF()
