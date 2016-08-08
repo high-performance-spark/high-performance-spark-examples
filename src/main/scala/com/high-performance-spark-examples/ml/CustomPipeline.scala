ab94221996611fed4f0e4889434b751af44290df
Holden Karau <holden@us.ibm.com>
2016-08-07 22:13:53 -0700

    Add a transform function and a configurable wordcount pipeline stage

--- src/main/scala/com/high-performance-spark-examples/ml/CustomPipeline.scala
+++ src/main/scala/com/high-performance-spark-examples/ml/CustomPipeline.scala
@@ -8,6 +8,8 @@ import scala.collection.mutable.{ArrayBuffer, MutableList}
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.ml._
 //tag::extraImports[]
@@ -16,7 +18,7 @@ import org.apache.spark.ml.util.Identifiable
 //end::extraImports[]
 
 //tag::basicPipelineSetup[]
-class HardCodedWordCountStage(override val uid: String) extends PipelineStage {
+class HardCodedWordCountStage(override val uid: String) extends Transformer {
   def this() = this(Identifiable.randomUID("hardcodedwordcount"))
 
   def copy(extra: ParamMap): HardCodedWordCountStage = {
@@ -36,4 +38,42 @@ class HardCodedWordCountStage(override val uid: String) extends PipelineStage {
     schema.add(StructField("happy_panda_counts", IntegerType, false))
   }
   //end::basicTransformSchema[]
+
+  //tag::transformFunction[]
+  def transform(df: Dataset[_]): DataFrame = {
+    val wordcount = udf { in: String => in.split(" ").size }
+    df.select(col("*"),
+      wordcount(df.col("happy_pandas")).as("happy_panda_counts"))
+  }
+  //end::transformFunction[]
+}
+
+
+//tag::paramTransformer[]
+class ConfigurableWordCount(override val uid: String) extends Transformer {
+  final val inputCol= new Param[String](this, "inputCol", "The input column")
+  final val outputCol = new Param[String](this, "outputCol", "The output column")
+//end::paramTransformer[]
+
+  def this() = this(Identifiable.randomUID("configurablewordcount"))
+
+  def copy(extra: ParamMap): HardCodedWordCountStage = {
+    defaultCopy(extra)
+  }
+
+  override def transformSchema(schema: StructType): StructType = {
+    // Check that the input type is a string
+    val idx = schema.fieldIndex($(inputCol))
+    val field = schema.fields(idx)
+    if (field.dataType != StringType) {
+      throw new Exception(s"Input type ${field.dataType} did not match input type StringType")
+    }
+    // Add the return field
+    schema.add(StructField($(outputCol), IntegerType, false))
+  }
+
+  def transform(df: Dataset[_]): DataFrame = {
+    val wordcount = udf { in: String => in.split(" ").size }
+    df.select(col("*"), wordcount(df.col($(inputCol))).as($(outputCol)))
+  }
 }
