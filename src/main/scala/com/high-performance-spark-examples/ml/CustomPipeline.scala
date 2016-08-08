a584143d0748ded6f1a13d12ecb3751847629b3c
Holden Karau <holden@us.ibm.com>
2016-08-07 23:48:59 -0700

    Add a simple string indexer example

--- src/main/scala/com/high-performance-spark-examples/ml/CustomPipeline.scala
+++ src/main/scala/com/high-performance-spark-examples/ml/CustomPipeline.scala
@@ -53,7 +53,10 @@ class HardCodedWordCountStage(override val uid: String) extends Transformer {
 class ConfigurableWordCount(override val uid: String) extends Transformer {
   final val inputCol= new Param[String](this, "inputCol", "The input column")
   final val outputCol = new Param[String](this, "outputCol", "The output column")
-//end::paramTransformer[]
+
+  def setInputCol(value: String): this.type = set(inputCol, value)
+
+  def setOutputCol(value: String): this.type = set(outputCol, value)
 
   def this() = this(Identifiable.randomUID("configurablewordcount"))
 
@@ -77,3 +80,73 @@ class ConfigurableWordCount(override val uid: String) extends Transformer {
     df.select(col("*"), wordcount(df.col($(inputCol))).as($(outputCol)))
   }
 }
+//end::paramTransformer[]
+
+
+//tag::simpleIndexer[]
+trait SimpleIndexerParams extends Params {
+  final val inputCol= new Param[String](this, "inputCol", "The input column")
+  final val outputCol = new Param[String](this, "outputCol", "The output column")
+}
+
+class SimpleIndexer(override val uid: String) extends
+    Estimator[SimpleIndexerModel] with SimpleIndexerParams {
+
+  def setInputCol(value: String): this.type = set(inputCol, value)
+
+  def setOutputCol(value: String): this.type = set(outputCol, value)
+
+  def this() = this(Identifiable.randomUID("simpleindexer"))
+
+  override def copy(extra: ParamMap): SimpleIndexer = {
+    defaultCopy(extra)
+  }
+
+  override def transformSchema(schema: StructType): StructType = {
+    // Check that the input type is a string
+    val idx = schema.fieldIndex($(inputCol))
+    val field = schema.fields(idx)
+    if (field.dataType != StringType) {
+      throw new Exception(s"Input type ${field.dataType} did not match input type StringType")
+    }
+    // Add the return field
+    schema.add(StructField($(outputCol), IntegerType, false))
+  }
+
+  override def fit(dataset: Dataset[_]): SimpleIndexerModel = {
+    import dataset.sparkSession.implicits._
+    val words = dataset.select(dataset($(inputCol)).as[String]).distinct
+      .collect()
+    new SimpleIndexerModel(uid, words)
+  }
+}
+
+class SimpleIndexerModel(
+  override val uid: String, words: Array[String])
+    extends Model[SimpleIndexerModel] with SimpleIndexerParams {
+
+  override def copy(extra: ParamMap): SimpleIndexerModel = {
+    defaultCopy(extra)
+  }
+
+  private val labelToIndex: Map[String, Double] = words.zipWithIndex.
+    map{case (x, y) => (x, y.toDouble)}.toMap
+
+  override def transformSchema(schema: StructType): StructType = {
+    // Check that the input type is a string
+    val idx = schema.fieldIndex($(inputCol))
+    val field = schema.fields(idx)
+    if (field.dataType != StringType) {
+      throw new Exception(s"Input type ${field.dataType} did not match input type StringType")
+    }
+    // Add the return field
+    schema.add(StructField($(outputCol), IntegerType, false))
+  }
+
+  override def transform(dataset: Dataset[_]): DataFrame = {
+    val indexer = udf { label: String => labelToIndex(label) }
+    dataset.select(col("*"),
+      indexer(dataset($(inputCol)).cast(StringType)).as($(outputCol)))
+  }
+}
+//end::SimpleIndexer[]
