08bd9ccd729ecff0b2219f519acd64a8fca0c1c8
Holden Karau <holden@us.ibm.com>
2016-10-01 16:28:58 -0700

    Add timer

--- src/main/scala/com/high-performance-spark-examples/streaming/Structured.scala
+++ src/main/scala/com/high-performance-spark-examples/streaming/Structured.scala
@@ -1,5 +1,7 @@
 package com.highperformancespark.examples.structuredstreaming
 
+import scala.concurrent.duration._
+
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming._
@@ -8,8 +10,14 @@ import org.apache.spark.sql.streaming._
 object Structured {
   def write(counts: Dataset[_]) = {
     //tag::writeComplete[]
-    val query = counts.writeStream.outputMode(OutputMode.Complete())
-      .format("parquet").queryName("pandas").start()
+    val query = counts.writeStream.
+      // Specify the output mode as Complete to support aggregations
+      outputMode(OutputMode.Complete()).
+      // Write out the result as parquet
+      format("parquet").
+      // Specify the interval at which new data will be picked up
+      trigger(ProcessingTime(1.second)).
+      queryName("pandas").start()
     //end::writeComplete[]
   }
 }
