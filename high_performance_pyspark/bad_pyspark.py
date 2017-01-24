3b3916c9d2f9eab7bd580fac6a04d583834f9479
Holden Karau <holden@us.ibm.com>
2017-01-24 15:18:30 -0800

    Add the bad python spark examples

---
+++ high_performance_pyspark/bad_pyspark.py
@@ -0,0 +1,118 @@
+# This script triggers a number of different PySpark errors
+
+from pyspark import *
+from pyspark.sql.session import SparkSession
+
+global sc
+
+def nonExistantInput(sc):
+    """
+    Attempt to load non existant input
+    >>> nonExistantInput(sc)
+    Traceback (most recent call last):
+        ...
+    Py4JJavaError:...
+    """
+    failedRdd = sc.textFile("file:///doesnotexist")
+    failedRdd.count()
+
+def throwOuter(sc):
+    """
+    Attempt to load non existant input
+    >>> throwOuter(sc)
+    Traceback (most recent call last):
+        ...
+    Py4JJavaError:...
+    """
+    data = sc.parallelize(range(10))
+    transform1 = data.map(lambda x: x + 1)
+    transform2 = transform1.map(lambda x: x / 0)
+    transform2.count()
+
+def throwInner(sc):
+    """
+    Attempt to load non existant input
+    >>> throwInner(sc)
+    Traceback (most recent call last):
+        ...
+    Py4JJavaError:...
+    """
+    data = sc.parallelize(range(10))
+    transform1 = data.map(lambda x: x / 0)
+    transform2 = transform1.map(lambda x: x + 1)
+    transform2.count()
+
+def add1(x):
+    """
+    Add 1
+    >>> add1(2)
+    3
+    """
+    return x + 1
+
+def divZero(x):
+    """
+    Divide by zero (cause an error)
+    >>> divZero(2)
+    Traceback (most recent call last):
+        ...
+    ZeroDivisionError: integer division or modulo by zero
+    """
+    return x / 0
+
+def throwOuter2(sc):
+    """
+    Attempt to load non existant input
+    >>> throwOuter2(sc)
+    Traceback (most recent call last):
+        ...
+    Py4JJavaError:...
+    """
+    data = sc.parallelize(range(10))
+    transform1 = data.map(add1)
+    transform2 = transform1.map(divZero)
+    transform2.count()
+
+def throwInner2(sc):
+    """
+    Attempt to load non existant input
+    >>> throwInner2(sc)
+    Traceback (most recent call last):
+        ...
+    Py4JJavaError:...
+    """
+    data = sc.parallelize(range(10))
+    transform1 = data.map(divZero)
+    transform2 = transform1.map(add1)
+    transform2.count()
+
+def _setupTest():
+    globs = globals()
+    spark = SparkSession.builder \
+                        .master("local[4]") \
+                        .getOrCreate()
+    sc = spark._sc
+    globs['sc'] = sc
+    return globs
+    
+def _test():
+    """
+    Run the tests. 
+    Note this will print a lot of error message to stderr since we don't capture the JVM sub process
+    stdout/stderr for doctests.
+    """
+    import doctest
+    globs = setupTest()
+    (failure_count, test_count) = doctest.testmod(globs=globs, optionflags=doctest.ELLIPSIS)
+    globs['sc'].stop()
+    if failure_count:
+        exit(-1)
+
+import sys
+if __name__ == "__main__":
+    _test()
+# Hack to support running in nose
+elif sys.stdout != sys.__stdout__:
+    _setupTest()
+else:
+    1 / 0
