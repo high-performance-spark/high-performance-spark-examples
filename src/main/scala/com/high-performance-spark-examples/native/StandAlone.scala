08969b67b7812dc986dc2dbece6a5cb3db9849d9
Jimin Hsieh <jimin.hsieh.engineer@gmail.com>
2019-09-17 09:57:43 +0000

    Ignore coverage of main method

--- src/main/scala/com/high-performance-spark-examples/native/StandAlone.scala
+++ src/main/scala/com/high-performance-spark-examples/native/StandAlone.scala
@@ -1,10 +1,12 @@
 package com.highperformancespark.examples.ffi
 
 object StandAlone {
+  // $COVERAGE-OFF$
   def main(args: Array[String]) {
     //tag::systemLoadLibrary[]
     System.loadLibrary("highPerformanceSpark0")
     //end::systemLoadLibrary[]
     println(new SumJNI().sum(Array(1,2,3)))
   }
+   // $COVERAGE-ON$
 }
