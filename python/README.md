b6a01da4da584b9ded426a56bb3908feed4ce6d3
Holden Karau <holden@pigscanfly.ca>
2023-03-28 19:28:37 -0700

    Add our first Python example (for dual writes).

---
+++ python/README.md
@@ -0,0 +1,1 @@
+Python examples for High Performance Spark
---
+++ python/examples/test_dual_write.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+from sparktestingbase.sqltestcase import SQLTestCase
+from pyspark.sql.functions import current_timestamp
+from pyspark.sql.types import Row
+from .dual_write import DualWriteExample
+
+
+class DualWriteTest(SQLTestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_actual_dual_write(self):
+        tempdir = tempfile.mkdtemp()
+        p1 = os.path.join(tempdir, "data1")
+        p2 = os.path.join(tempdir, "data2")
+        df = self.sqlCtx.createDataFrame([Row("timbit"), Row("farted")], ["names"])
+        combined = df.withColumn("times", current_timestamp())
+        DualWriteExample().do_write(combined, p1, p2)
+        df1 = self.sqlCtx.read.format("parquet").load(p1)
+        df2 = self.sqlCtx.read.format("parquet").load(p2)
+        self.assertDataFrameEqual(df2.select("times"), df1, 0.1)
