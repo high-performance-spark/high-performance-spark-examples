9828657f14e44eb9bf0b7bb6f6bd8a4c390e6f2d
Holden Karau <holden@pigscanfly.ca>
2023-09-21 11:08:19 -0700

    Add workaround

---
+++ sql/iceberg-schema-evolution-gotcha-workaround.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS local.udevelopers_sorted;
+CREATE TABLE IF NOT EXISTS local.udevelopers_sorted (
+       username string,
+       firstname string,
+       lastname string)
+USING ICEBERG;
+ALTER TABLE local.udevelopers_sorted WRITE ORDERED BY lastname;
+SELECT * FROM local.udevelopers_sorted;
+ALTER TABLE local.udevelopers_sorted WRITE ORDERED BY username;
+-- Hack, add it to identifier fields so we can do a "partial" drop where it stays in the schema and we don't
+-- corrupt the metadata.
+ALTER TABLE local.udevelopers_sorted ADD PARTITION FIELD lastname;
+ALTER TABLE local.udevelopers_sorted DROP PARTITION FIELD lastname;
+SELECT * FROM local.udevelopers_sorted;
