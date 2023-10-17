270804f67f7455c8d4159bc67292ce5fa0df1eac
Holden Karau <holden@pigscanfly.ca>
2023-10-16 22:01:03 -0700

    Fix iceberg setup and also fix the WAP SQL (+ file upstream bug) (#119)

--- sql/wap.sql
+++ sql/wap.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS local.projects (
+DROP TABLE IF EXISTS local.wap_projects;
+CREATE TABLE local.wap_projects (
        creator string,
        projectname string)
 USING iceberg
@@ -6,9 +7,13 @@ PARTITIONED BY (creator);
 ALTER TABLE local.projects SET TBLPROPERTIES (
     'write.wap.enabled''true'
 );
-ALTER TABLE local.projects CREATE BRANCH IF NOT EXISTS `audit`;
-SET spark.wap.branch = 'branch';
+-- We need a first commit, see https://github.com/apache/iceberg/issues/8849
+INSERT INTO local.wap_projects VALUES("holdenk", "spark");
+ALTER TABLE local.wap_projects DROP BRANCH IF EXISTS `audit-branch`;
+ALTER TABLE local.wap_projects CREATE BRANCH `audit-branch`;
+SET spark.wap.branch = 'audit-branch';
 INSERT INTO local.projects VALUES("krisnova", "aurae");
-SELECT count(*) FROM local.projects VERSION AS OF 'audit' WHERE creator is NULL;
--- This does not work until we upgrade to 3.5 + Iceberg 1.4.
--- CALL local.system.fastForward("local.projects", "main", "audit-branch");
+SELECT count(*) FROM local.wap_projects VERSION AS OF 'audit-branch' WHERE creator is NULL;
+SELECT count(*) FROM local.wap_projects VERSION AS OF 'audit-branch' WHERE creator == "krisnova";
+CALL local.system.remove_orphan_files(table => 'local.wap_projects');
+CALL local.system.fast_forward("local.wap_projects", "main", "audit-branch");
