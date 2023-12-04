39a28f2843caf36cc9d6c4ccaa6adb74eb3bf810
Holden Karau <holden@pigscanfly.ca>
2023-12-04 15:42:49 -0800

    Give up on Gluten on modern systems because I don't have time for that. But maybe we can get it to work with Clickhouse

--- setup_gluten.sh
+++ setup_gluten.sh
@@ -2,15 +2,23 @@
 set -ex
 
 # Setup deps
-sudo apt-get update && sudo apt-get install -y locales wget tar tzdata git ccache cmake ninja-build build-essential llvm-dev clang libiberty-dev libdwarf-dev libre2-dev libz-dev libssl-dev libboost-all-dev libcurl4-openssl-dev maven rapidjson-dev libdouble-conversion-dev libgflags-dev libsodium-dev libsnappy-dev && sudo apt install -y libunwind-dev && sudo apt-get install -y libgoogle-glog-dev
+sudo apt-get update && sudo apt-get install -y locales wget tar tzdata git ccache cmake ninja-build build-essential llvm-dev clang libiberty-dev libdwarf-dev libre2-dev libz-dev libssl-dev libboost-all-dev libcurl4-openssl-dev maven rapidjson-dev libdouble-conversion-dev libgflags-dev libsodium-dev libsnappy-dev nasm && sudo apt install -y libunwind-dev && sudo apt-get install -y libgoogle-glog-dev
 
-# Build gluten
+# Try gluten w/clickhouse
 if [ ! -d gluten ]; then
-  # We need Spark 3.5 w/scala212
-  git clone git@github.com:holdenk/gluten.git || git clone https://github.com/holdenk/gluten.git
+  git clone https://github.com/oap-project/gluten.git
   cd gluten
-  git checkout add-spark35-scala213
-  ./dev/builddeps-veloxbe.sh
-  mvn clean package -Pbackends-velox -Pspark-3.5 -DskipTests
-  cd ..
+  bash ./ep/build-clickhouse/src/build_clickhouse.sh
 fi
+
+# Setting up gluten with velox on modern ubuntu does not work
+# Build gluten
+#if [ ! -d gluten ]; then
+#  # We need Spark 3.5 w/scala212
+#  git clone git@github.com:holdenk/gluten.git || git clone https://github.com/holdenk/gluten.git
+#  cd gluten
+#  git checkout add-spark35-scala213
+#  ./dev/builddeps-veloxbe.sh
+#  mvn clean package -Pbackends-velox -Pspark-3.5 -DskipTests
+#  cd ..
+#fi
