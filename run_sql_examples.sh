d711be40bc0cf598a3c1ffabcd0f8dd7c85dd2df
Holden Karau <holden@pigscanfly.ca>
2023-12-04 17:14:51 -0800

    Work on trying to get something with gluten to run.

--- run_sql_examples.sh
+++ run_sql_examples.sh
@@ -9,21 +9,23 @@ GLUTEN_NATIVE_LIB_NAME=libhigh-performance-spark-gluten-0.so
 NATIVE_LIB_DIR=./native/src/
 NATIVE_LIB_PATH="${NATIVE_LIB_DIR}${GLUTEN_NATIVE_LIB_NAME}"
 GLUTEN_HOME=./gluten
-if [ -f "${NATIVE_LIB_PATH}" ]; then
+if [ -d ${GLUTEN_HOME} ]; then
   GLUTEN_EXISTS="true"
   gluten_jvm_jar=$(ls "${GLUTEN_HOME}"/package/target/gluten-velox-bundle-spark3.5_2.12-ubuntu_*-*-SNAPSHOT.jar) #TBD
   gluten_jvm_package_jar=$(ls "${GLUTEN_HOME}"/package/target/gluten-package*-*-SNAPSHOT.jar)
   GLUTEN_SPARK_EXTRA="--conf spark.plugins=io.glutenproject.GlutenPlugin \
   --conf spark.memory.offHeap.enabled=true \
-  --conf spark.memory.offHeap.size=20g \
-  --jars ${gluten_jvm_jar},${gluten_jvm_package_jar} \
-  --conf spark.jars=${gluten_jvm_jar} \
-  --conf spark.driver.extraClassPath=${gluten_jvm_package_jar} \
-  --conf spark.executor.extraClassPath=${gluten_jvm_package_jar} \
+  --conf spark.memory.offHeap.size=5g \
   --conf spark.shuffle.manager=org.apache.spark.shuffle.sort.ColumnarShuffleManager \
-  --conf spark.gluten.sql.columnar.backend.velox.udfLibraryPaths=${GLUTEN_NATIVE_LIB_NAME} \
-  --conf spark.gluten.loadLibFromJar=true \
-  --files ${NATIVE_LIB_PATH}"
+  --conf spark.gluten.sql.columnar.backend.velox.udfLibraryPaths=${GLUTEN_NATIVE_LIB_NAME}"
+  # Enable UDF seperately.
+  if [ -f "${NATIVE_LIB_PATH}" ]; then
+    GLUTEN_SPARK_EXTRA="$GLUTEN_SPARK_EXTRA \
+     --jars ${gluten_jvm_jar},${gluten_jvm_package_jar} \
+     --conf spark.jars=${gluten_jvm_jar} \
+     --conf spark.gluten.loadLibFromJar=true \
+     --files ${NATIVE_LIB_PATH}"
+  fi
 fi
 
 function run_example () {
