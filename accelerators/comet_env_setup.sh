1fe48976cb255a600cda1725a9555d8fa3436fc9
Holden Karau <holden@pigscanfly.ca>
2024-03-20 11:22:41 -0700

    Turn on Comet shuffle

--- accelerators/comet_env_setup.sh
+++ accelerators/comet_env_setup.sh
@@ -3,7 +3,10 @@
 SPARK_EXTRA="--jars ${COMET_JAR} \
 --conf spark.comet.enabled=true \
 --conf spark.comet.exec.enabled=true \
---conf spark.comet.exec.all.enabled=true"
+--conf spark.comet.exec.all.enabled=true \
+--conf spark.shuffle.manager=org.apache.spark.sql.comet.execution.shuffle.CometShuffleManager \
+--conf spark.comet.exec.shuffle.enabled=true \
+--conf spark.comet.columnar.shuffle.enabled=true"
 # Instead of --conf spark.sql.extensions=org.apache.comet.CometSparkSessionExtensions we set
 # EXTRA_EXTENSIONS so it can be appended to iceberg
 if [ -z "$EXTRA_EXTENSIONS" ]; then
