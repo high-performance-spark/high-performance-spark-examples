8354fe752a1e9a446b19a8f03efd62190b5119d5
Holden Karau <holden@pigscanfly.ca>
2024-03-24 17:45:51 -0700

    Make setup_gluten_deps better formated for book inclusion

--- accelerators/setup_gluten_deps.sh
+++ accelerators/setup_gluten_deps.sh
@@ -2,7 +2,13 @@
 set -ex
 
 sudo apt-get update
-sudo apt-get install -y locales wget tar tzdata git ccache cmake ninja-build build-essential llvm-dev clang libiberty-dev libdwarf-dev libre2-dev libz-dev libssl-dev libboost-all-dev libcurl4-openssl-dev maven rapidjson-dev libdouble-conversion-dev libgflags-dev libsodium-dev libsnappy-dev nasm && sudo apt install -y libunwind-dev
+#tag::gluten_deps[]
+sudo apt-get install -y locales wget tar tzdata git ccache cmake ninja-build build-essential \
+     llvm-dev clang libiberty-dev libdwarf-dev libre2-dev libz-dev libssl-dev libboost-all-dev \
+     libcurl4-openssl-dev maven rapidjson-dev libdouble-conversion-dev libgflags-dev \
+     libsodium-dev libsnappy-dev nasm
+sudo apt install -y libunwind-dev
 sudo apt-get install -y libgoogle-glog-dev
 sudo apt-get -y install docker-compose
-sudo apt-get install -y libre2-9
+sudo apt-get install -y libre2-9 || sudo apt-get install -y libre2-10
+#end::gluten_deps[]
