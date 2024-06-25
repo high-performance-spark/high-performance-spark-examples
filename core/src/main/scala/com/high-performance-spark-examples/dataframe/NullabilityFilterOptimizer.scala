7120b2bd1d9de972c47ecd13301a6da7db979928
Holden Karau <holden@pigscanfly.ca>
2024-06-25 16:14:20 -0700

    Sql extension plugin optimizer (#128)
    * WIP: Start adding a sample rule to introduce null filters for non-nullable magic. Note we have no tests but it compiles.
    
    * Redirect stedder as well
    
    * Fix the custom sample optimizer, switch to using transformUp instead of transformWithPruning
    
    * We don't care about bottom up or top down for this one
    
    * 86 the old way

---
+++ core/src/main/scala/com/high-performance-spark-examples/dataframe/NullabilityFilterOptimizer.scala
@@ -0,0 +1,28 @@
+/**
+ * Extension for the SparkSession to allow us to plug in a custom optimizer
+ */
+
+package com.highperformancespark.examples.dataframe
+
+import org.apache.spark.sql.catalyst.optimizer._
+import org.apache.spark.sql.catalyst.plans.logical._
+import org.apache.spark.sql.catalyst.rules.Rule
+import org.apache.spark.sql.catalyst.trees.TreePattern._
+import org.apache.spark.sql.catalyst.expressions.{And, IsNotNull, NullIntolerant}
+
+object NullabilityFilterOptimizer extends Rule[LogicalPlan] {
+
+  def apply(plan: LogicalPlan): LogicalPlan = {
+    plan.transform {
+      case p @ Project(projectList, projChild) =>
+        val children = projectList.flatMap(_.children)
+        // If there are no null intolerant children don't worry about it
+        if (children.isEmpty) {
+          p
+        } else {
+          val filterCond = children.map(IsNotNull(_)).reduceLeft(And)
+          Project(projectList, Filter(filterCond, projChild))
+        }
+    }
+  }
+}
---
+++ core/src/main/scala/com/high-performance-spark-examples/dataframe/SQLExtension.scala
@@ -0,0 +1,15 @@
+/**
+ * Extension for the SparkSession to allow us to plug in a custom optimizer
+ */
+
+package com.highperformancespark.examples.dataframe
+
+import org.apache.spark.sql.{SparkSessionExtensions, SparkSessionExtensionsProvider}
+
+class SQLExtension extends SparkSessionExtensionsProvider {
+  override def apply(extensions: SparkSessionExtensions): Unit = {
+    // There are _many different_ types of rules you can inject, here we're focused on
+    // making things go fast so our sample is an optimizer rule (AQE rules could also make sense).
+    extensions.injectOptimizerRule(session => NullabilityFilterOptimizer)
+  }
+}
