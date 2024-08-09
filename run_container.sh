d73eafd7550b08cbe70327f1f5fe6c3f5aa1b2f3
Holden Karau <holden@pigscanfly.ca>
2024-08-09 11:44:55 -0700

    More shellcheck fixes

--- run_container.sh
+++ run_container.sh
@@ -4,4 +4,4 @@ set -ex
 docker image pull holdenk/hps:0.1
 mkdir -p warehouse
 mkdir -p iceberg-workshop
-docker container  run --mount type=bind,source="$(pwd)"/warehouse,target=/warehouse --mount type=bind,source="$(pwd)/iceberg-workshop",target="/high-performance-spark-examples/iceberg-workshop" -p 8877:8877 -it holdenk/hps:0.1 /bin/bash
+docker container  run --mount type=bind,source="$(pwd)"/warehouse,target=/warehouse --mount type=bind,source="$(pwd)/iceberg-workshop",target=/high-performance-spark-examples/iceberg-workshop -p 8877:8877 -it holdenk/hps:0.1 /bin/bash
