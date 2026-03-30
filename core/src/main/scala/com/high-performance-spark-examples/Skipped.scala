7458799723e3dd9ef820f417cd342d0137732c0a
Holden Karau <holden@pigscanfly.ca>
2026-03-29 20:37:50 -0700

    Import sc._ and repart

--- core/src/main/scala/com/high-performance-spark-examples/Skipped.scala
+++ core/src/main/scala/com/high-performance-spark-examples/Skipped.scala
@@ -7,14 +7,16 @@ import org.apache.spark._
 import org.apache.spark.rdd.RDD
 
 object Skipped {
-  def shuffleSkip(sc: SparkContext, inputRDD: RDD[String]): RDD[String] = {
+  def shuffleSkip(sc: SparkContext, inputRDD: RDD[String]): RDD[_] = {
+    import sc._
     val byLength = inputRDD.map(x => (x.length, x))
     val grouped = byLength.groupByKey()
-    val groupedRepart = grouped.repartition(10).map((x, k) => k.length)
+    val groupedRepart = grouped.repartition(10).mapValues(x => x.toList.length)
     // Create the shuffle file
     val countGrouped = groupedRepart.count()
-    val repart = grouped.repartiton(10)
-    val combined = countGrouped.join(repart)
+    val repart = grouped.repartition(10)
+    repart.count()
+    val combined = groupedRepart.join(repart)
     combined
   }
 }
